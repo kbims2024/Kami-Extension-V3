860d0ceaecf803f6d77b92f2a93663666a797bab
Z User <z@container>
2026-08-03 08:57:21 +0000

    9259c9b1-994e-46b4-985f-851b2491822d

--- keepalive.cjs
+++ keepalive.cjs
@@ -1,15 +1,17 @@
 const { spawn } = require('child_process');
 const http = require('http');
 
-const next = spawn('npx', ['next', 'dev', '-p', '3000'], {
+const next = spawn('bun', ['run', 'dev'], {
   stdio: ['ignore', 'pipe', 'pipe'],
-  shell: true,
   cwd: '/home/z/my-project',
   env: { ...process.env }
 });
 next.stdout.on('data', d => process.stdout.write(d));
 next.stderr.on('data', d => process.stderr.write(d));
-next.on('exit', () => process.exit(1));
+next.on('exit', (code) => {
+  console.error('Next.js exited with code:', code);
+  process.exit(1);
+});
 
 setInterval(() => {
   http.get('http://localhost:3000/', (res) => res.on('data', () => {})).on('error', () => {});
